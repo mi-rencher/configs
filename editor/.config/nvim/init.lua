24e260239c6e3881d551c48df550d177c8d68b42
Michael Rencher <renchermp@gmail.com>
2026-03-25 13:17:30 -0600

    indent plugin and some more keymaps

--- editor/.config/nvim/init.lua
+++ editor/.config/nvim/init.lua
@@ -21,12 +21,15 @@ vim.opt.relativenumber = true
 vim.opt.fileformats = { "unix", "dos" }
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-
+vim.opt.scrolloff = 8
 vim.opt.swapfile = false
 
-vim.keymap.set('n', '<c-a>', ':Neotree filesystem reveal float<cr>')
+-- KEYMAPS
 
--- misc keymaps
+-- Config
+vim.keymap.set('n', '<leader>r', ':source ~/.config/nvim/init.lua<cr>', { desc = "Reload config" })
+vim.keymap.set("v", "<Space>", "<Nop>", { silent = true }) -- space leader works in visual mode
+vim.keymap.set('n', '<c-a>', ':Neotree filesystem reveal float<cr>')
 
 -- Control+s to save file and return to normal mode
 vim.keymap.set('n', '<c-s>', ':w<cr>')
@@ -35,11 +38,27 @@ vim.keymap.set('i', '<c-s>', '<Esc>:w<cr>')
 vim.keymap.set('n', '<c-q>', ':q!<cr>')
 vim.keymap.set('i', '<c-q>', '<Esc>:q!<cr>')
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<cr>') -- space+h to clear find highlight text
+-- Search
+vim.keymap.set('n', '<CR>', ':nohlsearch<cr>') -- clear hightlight text on enter
+vim.keymap.set('n', '<leader>h', ':nohlsearch<cr>') -- space+h to clear find highlight texj
+vim.keymap.set('n', 'n', 'nzz')
+vim.keymap.set('n', 'N', 'Nzz')
+vim.keymap.set('v', '/', '"hy/\\V<C-r>h<CR>', { desc = "Search for selection (literal)", silent = false })
+
+-- Navigation
 vim.keymap.set('n', 'B', '^') -- shift+b jump to beginning of line
 vim.keymap.set('n', 'E', '$') -- shift+e jumpt to end of line
-vim.keymap.set('n', '<leader>r', ':source ~/.config/nvim/init.lua<cr>', { desc = "Reload config" })
+-- vim.keymap.set('n', '<C-j>', '<C-d>zz') -- These were interfering with window navigation, because I'm a dummy...
+-- vim.keymap.set('n', '<C-k>', '<C-u>zz')
+
+-- Visual Mode
+vim.keymap.set('v', '<CR>', '<Esc>')
+
+-- Gsub
+vim.keymap.set('n', '<C-d>', '"hyiw:%s/<C-r>h//gc<left><left><left>', { desc = "Substitute word under cursor", silent = false })
 vim.keymap.set('v', '<C-d>', '"hy:%s/<C-r>h//gc<left><left><left>', { desc = "Substitute with selection", silent = false })
+
+-- Indents
 vim.keymap.set('v', '>', '>gv', { desc = "Indent selection right and reselect" })
 vim.keymap.set('v', '<', '<gv', { desc = "Indent selection left and reselect" })
 
@@ -52,4 +71,15 @@ vim.keymap.set('i', '<m-j>', '<Esc>:m .+1<CR>==gi', { desc = 'Move line down' })
 
 vim.keymap.set('v', '<m-k>', ":m '<-2<CR>gv=gv", { desc = 'Move lines up' })
 vim.keymap.set('v', '<m-j>', ":m '>+1<CR>gv=gv", { desc = 'Move lines down' })
+
+-- Window controls
+vim.keymap.set('n', '<C-w>-', ':split<CR>')
+vim.keymap.set('n', '<C-w>|', ':vsplit<CR>')
+vim.keymap.set('n', '<C-w>x', ':close<CR>')
+vim.keymap.set('n', '<C-w>k', ':only<CR>')
+
+vim.keymap.set('n', '<m-=>', ':resize +10<CR>')
+vim.keymap.set('n', '<m-->', ':resize -10<CR>')
+vim.keymap.set('n', '<m-]>', ':vertical resize +10<CR>')
+vim.keymap.set('n', '<m-[>', ':vertical resize -10<CR>')
 
--- editor/.config/nvim/lua/plugins/telescope.lua
+++ editor/.config/nvim/lua/plugins/telescope.lua
@@ -7,6 +7,14 @@ return {
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			-- vim.keymap.set("v", "<leader>fg", builtin.live_grep, {})
+      -- vim.keymap.set("v", "<leader>fg", function()
+      --     local saved = vim.fn.getreg('"')
+      --     vim.cmd('noau normal! y')
+      --     local selection = vim.fn.getreg('"')
+      --     vim.fn.setreg('"', saved)
+      --     builtin.live_grep({ default_text = selection })
+      -- end, { desc = "Live grep selection" })
 		end,
 	},
 	{
